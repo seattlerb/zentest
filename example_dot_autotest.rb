8753c87de0d2b49fde8a2529153f66574e6a95f2
Ryan Davis <ryand@zenspider.com>
2007-03-20 03:24:10 -0800

    Added autotest plugin to update the GNU screen statusbar. Added autotest plugin to update ichat/adium IM status with code stats.
    [git-p4: depot-paths = "//src/ZenTest/dev/": change = 3041]

--- example_dot_autotest.rb
+++ example_dot_autotest.rb
@@ -1,10 +1,12 @@
 # -*- ruby -*-
 
+# require 'autotest/fixtures'
 # require 'autotest/growl'
 # require 'autotest/html_report'
 # require 'autotest/kdenotify'
 # require 'autotest/menu'
 # require 'autotest/pretty'
 # require 'autotest/redgreen'
+# require 'autotest/shame'
 # require 'autotest/snarl'
 # require 'autotest/timestamp'
