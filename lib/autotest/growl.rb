1c52d29ff6586c49967a169aa0e4368ec63e648e
Ryan Davis <ryand@zenspider.com>
2007-03-21 01:37:59 -0800

    Improved output even more. Most of the messages were useless (only available when you're already interacting with it). Also fixed the interrupt thing.
    [git-p4: depot-paths = "//src/ZenTest/dev/": change = 3045]

--- lib/autotest/growl.rb
+++ lib/autotest/growl.rb
@@ -1,16 +1,14 @@
 # -*- mode -*-
 
 module Autotest::Growl
-  require 'fileutils'
-
   def self.growl title, msg, pri=0
-    title = title + " in #{FileUtils.pwd}"
-      msg = msg + " at #{Time.now}"
+    title += " in #{Dir.pwd}"
+    msg += " at #{Time.now}"
     system "growlnotify -n autotest --image /Applications/Mail.app/Contents/Resources/Caution.tiff -p #{pri} -m #{msg.inspect} #{title}"
   end
 
   Autotest.add_hook :run do  |at|
-    growl "autotest running", "Started" unless $TESTING
+    growl "autotest running", "Started"
   end
 
   Autotest.add_hook :red do |at|
@@ -18,22 +16,10 @@ module Autotest::Growl
   end
 
   Autotest.add_hook :green do |at|
-    growl "Tests Passed", "All tests passed", -2 if at.tainted 
+    growl "Tests Passed", "Tests passed", -2 if at.tainted
   end
 
-  Autotest.add_hook :init do |at|
-    growl "autotest", "autotest was started" unless $TESTING
-  end
-
-  Autotest.add_hook :interrupt do |at|
-    growl "autotest", "autotest was reset" unless $TESTING
-  end
-
-  Autotest.add_hook :quit do |at|
-    growl "autotest", "autotest is exiting" unless $TESTING
-  end
-
-  Autotest.add_hook :all do |at|_hook
-    growl "autotest", "Tests have fully passed", -2 unless $TESTING
+  Autotest.add_hook :all_good do |at|
+    growl "Tests Passed", "All tests passed", -2 if at.tainted
   end
 end
