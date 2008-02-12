f248912d0f3aa3f60587f5bc054a731f2046f534
Ryan Davis <ryand@zenspider.com>
2008-02-12 12:29:33 -0800

    Fully parameterized
    [git-p4: depot-paths = "//src/ZenTest/dev/": change = 3828]

--- lib/autotest/rcov.rb
+++ lib/autotest/rcov.rb
@@ -1,12 +1,16 @@
 module Autotest::RCov
-  @@pattern = "test/*.rb"
+  @@command, @@pattern = "rcov", "test/*.rb"
+
+  def self.command= o
+    @@command = o
+  end
 
   def self.pattern= o
     @@pattern = o
   end
 
   Autotest.add_hook :all_good do |at|
-    system "rake rcov_info PATTERN=test/test_ruby_lexer.rb"
+    system "rake #{@@command} PATTERN=#{@@pattern}"
   end
 end
 
