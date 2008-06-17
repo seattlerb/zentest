c932511976ec369cde88877e3361c5d9e1f08876
Ryan Davis <ryand@zenspider.com>
2008-06-17 15:49:13 -0800

    Sortied methods
    [git-p4: depot-paths = "//src/ZenTest/dev/": change = 4017]

--- lib/multiruby.rb
+++ lib/multiruby.rb
@@ -34,209 +34,6 @@ module Multiruby
   TAGS     = %w(    1_8_6 1_8_7 1_9  ).map { |v| "tag:#{v}" }
   BRANCHES = %w(1_8 1_8_6 1_8_7 trunk).map { |v| "branch:#{v}" }
 
-  def self.help
-    File.readlines(__FILE__).each do |line|
-      next unless line =~ /^#( |$)/
-      puts line.sub(/^# ?/, '')
-    end
-  end
-
-  def self.list
-    puts "Known versions:"
-    in_install_dir do
-      Dir["*"].sort.each do |d|
-        puts "  #{d}"
-      end
-    end
-  end
-
-  def self.rm name
-    Multiruby.in_root_dir do
-      FileUtils.rm_rf Dir["*/#{name}"]
-      File.unlink "versions/ruby-#{name}.tar.gz"
-    end
-  end
-
-  def self.each_scm_build_dir
-    Multiruby.in_build_dir do
-      Dir["*"].each do |dir|
-        next unless File.directory? dir
-        Dir.chdir dir do
-          if File.exist?(".svn") || File.exist?(".git") then
-            scm = File.exist?(".svn") ? :svn : :git
-            yield scm
-          else
-            yield :none
-          end
-        end
-      end
-    end
-  end
-
-  def self.clean
-    self.each_scm_build_dir do |style|
-      case style
-      when :svn, :git then
-        if File.exist? "Makefile" then
-          system "make clean"
-        elsif File.exist? "Rakefile" then
-          system "rake clean"
-        end
-      else
-        FileUtils.rm_rf Dir.pwd
-      end
-    end
-  end
-
-  def self.update
-    # TODO:
-    # update will look at the dir name and act accordingly rel_.* will
-    # figure out latest tag on that name and svn sw to it trunk and
-    # others will just svn update
-
-    self.each_scm_build_dir do |style|
-      case style
-      when :svn then
-        dir = File.basename(Dir.pwd)
-        warn dir
-        case dir
-        when /mri_\d/ then
-          system "svn cleanup" # just in case
-          FileUtils.rm_rf "../install/#{dir}" if `svn up` =~ /^[ADUCG] /
-        when /tag/
-          warn "don't know how to update tags: #{dir}"
-          # url = `svn info`[/^URL: (.*)/, 1]
-        else
-          warn "don't know how to update: #{dir}"
-        end
-      else
-        warn "update in non-svn dir not supported yet: #{dir}"
-      end
-    end
-  end
-
-  def self.update_rubygems
-    url = "http://files.rubyforge.rubyuser.de/rubygems/"
-    html = URI.parse(url).read
-
-    versions = html.scan(/href="rubygems-update-(\d+(?:\.\d+)+).gem/).flatten
-    latest = versions.sort_by { |s| s.scan(/\d+/).map { |s| s.to_i } }.last
-
-    Multiruby.in_versions_dir do
-      File.unlink(*Dir["rubygems*"])
-      file = "rubygems-#{latest}.tgz"
-      File.open file, 'w' do |f|
-        f.write URI.parse(url+file).read
-      end
-    end
-  end
-
-  def self.tags
-    tags = nil
-    Multiruby.in_tmp_dir do
-      cache = "svn.tag.cache"
-      File.unlink cache if Time.now - File.mtime(cache) > 86400 rescue nil
-
-      File.open cache, "w" do |f|
-        f.write `svn ls #{MRI_SVN}/tags/`
-      end unless File.exist? cache
-
-      tags = File.read(cache).split(/\n/).grep(/^v/).reject {|s| s =~ /preview/}
-    end
-
-    tags = tags.sort_by { |s| s.scan(/\d+/).map { |s| s.to_i } }
-  end
-
-  def self.run(cmd)
-    puts "Running command: #{cmd}"
-    raise "ERROR: Command failed with exit code #{$?}" unless system cmd
-  end
-
-  def self.root_dir
-    root_dir = File.expand_path(ENV['MULTIRUBY'] ||
-                                File.join(ENV['HOME'], ".multiruby"))
-
-    unless test ?d, root_dir then
-      puts "creating #{root_dir}"
-      Dir.mkdir root_dir, 0700
-    end
-
-    root_dir
-  end
-
-  def self.in_root_dir
-    Dir.chdir self.root_dir do
-      yield
-    end
-  end
-
-  def self.in_install_dir
-    Dir.chdir File.join(self.root_dir, "install") do
-      yield
-    end
-  end
-
-  def self.in_build_dir
-    Dir.chdir File.join(self.root_dir, "build") do
-      yield
-    end
-  end
-
-  def self.in_versions_dir
-    Dir.chdir File.join(self.root_dir, "versions") do
-      yield
-    end
-  end
-
-  def self.in_tmp_dir
-    Dir.chdir File.join(self.root_dir, "tmp") do
-      yield
-    end
-  end
-
-  def self.extract_latest_version url
-    file = URI.parse(url).read
-    versions = file.scan(/href="(ruby.*tar.gz)"/).flatten.reject { |s|
-      s =~ /preview/
-    }.sort_by { |s|
-      s.split(/\D+/).map { |i| i.to_i }
-    }.flatten.last
-  end
-
-  def self.fetch_tar v
-    require 'open-uri'
-    base_url = "http://ftp.ruby-lang.org/pub/ruby"
-
-    in_versions_dir do
-      warn "    Determining latest version for #{v}"
-      base = extract_latest_version("#{base_url}/#{v}/")
-      url = File.join base_url, v, base
-      warn "    Fetching #{base} via HTTP... this might take a while."
-      open(url) do |f|
-        File.open base, 'w' do |out|
-          out.write f.read
-        end
-      end
-    end
-  end
-
-  def self.setup_dirs
-    %w(build install versions tmp).each do |dir|
-      unless test ?d, dir then
-        puts "creating #{dir}"
-        Dir.mkdir dir
-        if dir == "versions" then
-          warn "  Downloading initial ruby tarballs to ~/.multiruby/versions:"
-          %w(1.8 1.9).each do |v|
-            self.fetch_tar v
-          end
-          warn "  ...done"
-          warn "  Put other ruby tarballs in ~/.multiruby/versions to use them."
-        end
-      end
-    end
-  end
-
   def self.build_and_install
     root_dir = self.root_dir
     versions = []
@@ -294,10 +91,213 @@ module Multiruby
     versions
   end
 
+  def self.clean
+    self.each_scm_build_dir do |style|
+      case style
+      when :svn, :git then
+        if File.exist? "Makefile" then
+          system "make clean"
+        elsif File.exist? "Rakefile" then
+          system "rake clean"
+        end
+      else
+        FileUtils.rm_rf Dir.pwd
+      end
+    end
+  end
+
+  def self.each_scm_build_dir
+    Multiruby.in_build_dir do
+      Dir["*"].each do |dir|
+        next unless File.directory? dir
+        Dir.chdir dir do
+          if File.exist?(".svn") || File.exist?(".git") then
+            scm = File.exist?(".svn") ? :svn : :git
+            yield scm
+          else
+            yield :none
+          end
+        end
+      end
+    end
+  end
+
+  def self.extract_latest_version url
+    file = URI.parse(url).read
+    versions = file.scan(/href="(ruby.*tar.gz)"/).flatten.reject { |s|
+      s =~ /preview/
+    }.sort_by { |s|
+      s.split(/\D+/).map { |i| i.to_i }
+    }.flatten.last
+  end
+
+  def self.fetch_tar v
+    require 'open-uri'
+    base_url = "http://ftp.ruby-lang.org/pub/ruby"
+
+    in_versions_dir do
+      warn "    Determining latest version for #{v}"
+      base = extract_latest_version("#{base_url}/#{v}/")
+      url = File.join base_url, v, base
+      warn "    Fetching #{base} via HTTP... this might take a while."
+      open(url) do |f|
+        File.open base, 'w' do |out|
+          out.write f.read
+        end
+      end
+    end
+  end
+
+  def self.help
+    File.readlines(__FILE__).each do |line|
+      next unless line =~ /^#( |$)/
+      puts line.sub(/^# ?/, '')
+    end
+  end
+
+  def self.in_build_dir
+    Dir.chdir File.join(self.root_dir, "build") do
+      yield
+    end
+  end
+
+  def self.in_install_dir
+    Dir.chdir File.join(self.root_dir, "install") do
+      yield
+    end
+  end
+
+  def self.in_root_dir
+    Dir.chdir self.root_dir do
+      yield
+    end
+  end
+
+  def self.in_tmp_dir
+    Dir.chdir File.join(self.root_dir, "tmp") do
+      yield
+    end
+  end
+
+  def self.in_versions_dir
+    Dir.chdir File.join(self.root_dir, "versions") do
+      yield
+    end
+  end
+
+  def self.list
+    puts "Known versions:"
+    in_install_dir do
+      Dir["*"].sort.each do |d|
+        puts "  #{d}"
+      end
+    end
+  end
+
+  def self.rm name
+    Multiruby.in_root_dir do
+      FileUtils.rm_rf Dir["*/#{name}"]
+      File.unlink "versions/ruby-#{name}.tar.gz"
+    end
+  end
+
+  def self.root_dir
+    root_dir = File.expand_path(ENV['MULTIRUBY'] ||
+                                File.join(ENV['HOME'], ".multiruby"))
+
+    unless test ?d, root_dir then
+      puts "creating #{root_dir}"
+      Dir.mkdir root_dir, 0700
+    end
+
+    root_dir
+  end
+
+  def self.run(cmd)
+    puts "Running command: #{cmd}"
+    raise "ERROR: Command failed with exit code #{$?}" unless system cmd
+  end
+
+  def self.setup_dirs
+    %w(build install versions tmp).each do |dir|
+      unless test ?d, dir then
+        puts "creating #{dir}"
+        Dir.mkdir dir
+        if dir == "versions" then
+          warn "  Downloading initial ruby tarballs to ~/.multiruby/versions:"
+          %w(1.8 1.9).each do |v|
+            self.fetch_tar v
+          end
+          warn "  ...done"
+          warn "  Put other ruby tarballs in ~/.multiruby/versions to use them."
+        end
+      end
+    end
+  end
+
   def self.svn_co url, dir
     Multiruby.in_versions_dir do
       Multiruby.run "svn co #{url} #{dir}" unless File.directory? dir
       FileUtils.ln_s "../versions/#{dir}", "../build/#{dir}"
+    end
+  end
+
+  def self.tags
+    tags = nil
+    Multiruby.in_tmp_dir do
+      cache = "svn.tag.cache"
+      File.unlink cache if Time.now - File.mtime(cache) > 86400 rescue nil
+
+      File.open cache, "w" do |f|
+        f.write `svn ls #{MRI_SVN}/tags/`
+      end unless File.exist? cache
+
+      tags = File.read(cache).split(/\n/).grep(/^v/).reject {|s| s =~ /preview/}
+    end
+
+    tags = tags.sort_by { |s| s.scan(/\d+/).map { |s| s.to_i } }
+  end
+
+  def self.update
+    # TODO:
+    # update will look at the dir name and act accordingly rel_.* will
+    # figure out latest tag on that name and svn sw to it trunk and
+    # others will just svn update
+
+    self.each_scm_build_dir do |style|
+      case style
+      when :svn then
+        dir = File.basename(Dir.pwd)
+        warn dir
+        case dir
+        when /mri_\d/ then
+          system "svn cleanup" # just in case
+          FileUtils.rm_rf "../install/#{dir}" if `svn up` =~ /^[ADUCG] /
+        when /tag/
+          warn "don't know how to update tags: #{dir}"
+          # url = `svn info`[/^URL: (.*)/, 1]
+        else
+          warn "don't know how to update: #{dir}"
+        end
+      else
+        warn "update in non-svn dir not supported yet: #{dir}"
+      end
+    end
+  end
+
+  def self.update_rubygems
+    url = "http://files.rubyforge.rubyuser.de/rubygems/"
+    html = URI.parse(url).read
+
+    versions = html.scan(/href="rubygems-update-(\d+(?:\.\d+)+).gem/).flatten
+    latest = versions.sort_by { |s| s.scan(/\d+/).map { |s| s.to_i } }.last
+
+    Multiruby.in_versions_dir do
+      File.unlink(*Dir["rubygems*"])
+      file = "rubygems-#{latest}.tgz"
+      File.open file, 'w' do |f|
+        f.write URI.parse(url+file).read
+      end
     end
   end
 end
