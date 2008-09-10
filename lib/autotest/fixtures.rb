b00d8e157a2778b56412d1b4c33918866e153754
Ryan Davis <ryand@zenspider.com>
2008-09-10 11:50:27 -0800

    Fixed lexical ambiguity.
    [git-p4: depot-paths = "//src/ZenTest/dev/": change = 4188]

--- lib/autotest/fixtures.rb
+++ lib/autotest/fixtures.rb
@@ -6,7 +6,7 @@
 module Autotest::Fixtures
   Autotest.add_hook :initialize do  |at|
     at.test_mappings['^test/fixtures/(.*)s.yml'] = proc { |filename, matches|
-      at.files_matching /test\/\w+\/#{matches[1]}(_\w+)?.*_test.rb$/
+      at.files_matching(/test\/\w+\/#{matches[1]}(_\w+)?.*_test.rb$/)
     }
   end
 end
