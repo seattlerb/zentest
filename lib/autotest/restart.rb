8c81a56094134ad5d29f1cfbc536d8f0f57ad0a0
Ryan Davis <ryand@zenspider.com>
2010-09-28 14:39:37 -0800

    - autotest/restart now passes ARGV back through to exec [git-p4: depot-paths = "//src/ZenTest/dev/": change = 5880]

--- lib/autotest/restart.rb
+++ lib/autotest/restart.rb
@@ -2,10 +2,11 @@ module Autotest::Restart
   Autotest.add_hook :updated do |at, *args|
     if args.flatten.include? ".autotest" then
       warn "Detected change to .autotest, restarting"
-      cmd = "autotest"
+      cmd = %w(autotest)
       cmd << " -v" if $v
-      
-      exec cmd
+      cmd += ARGV
+
+      exec *cmd
     end
   end
 end
