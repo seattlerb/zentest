3b0432ecf2ead2aa41fbd2d749f0c8861f64df56
bhenderson <bhenderson@attinteractive.com>
2011-08-11 11:37:44 -0700

    update hook args is now an array of hashes. Add default configs to watched files.

--- lib/autotest/restart.rb
+++ lib/autotest/restart.rb
@@ -1,6 +1,12 @@
 module Autotest::Restart
+  Autotest.add_hook :initialize do |at|
+    configs = [File.expand_path('~/.autotest'), './.autotest']
+    at.extra_files.concat configs
+    false
+  end
+
   Autotest.add_hook :updated do |at, *args|
-    if args.flatten.include? ".autotest" then
+    if args.any? {|h| h.keys.grep /\.autotest$/} then
       warn "Detected change to .autotest, restarting"
       at.restart
     end
